cdf07b311d78ad2279277f2c96ee304cbe8ee399
Michael Naples <naplesm96@gmail.com>
2024-12-02 21:11:12 -0500

    Cleaned up day 1.

--- src/day1.py
+++ src/day1.py
@@ -5,34 +5,27 @@ with open('../data/day1.txt', 'r') as f:
 
 input_arr = file.splitlines()
 
-left, right = [], []
-for line in input_arr:
-    s = line.split()
-    left.append(int(s[0]))
-    right.append(int(s[1]))
-
-left, right = np.sort(np.asarray(left)), np.sort(np.asarray(right))
-
-distances = np.abs(left - right)
-
-print('Part 1:', np.sum(distances))
-
-# Part 2
-
-left, right = [], {}
+left, right, right_dict = [], [], {}
 for line in input_arr:
     s = line.split()
     left.append(int(s[0]))
     r = int(s[1])
-    if r in right.keys():
-        right[r] += 1
+    right.append(r)
+    if r in right_dict.keys():
+        right_dict[r] += 1
     else:
-        right[r] = 1
+        right_dict[r] = 1
 
+# Part 1
+left, right = np.sort(np.asarray(left)), np.sort(np.asarray(right))
+distances = np.abs(left - right)
+print('Part 1:', np.sum(distances))
+
+# Part 2
 similarities = []
 for num in left:
-    if num in right.keys():
-        similarities.append(num * right[num])
+    if num in right_dict.keys():
+        similarities.append(num * right_dict[num])
 
 print('Part 2:', sum(similarities))
 
